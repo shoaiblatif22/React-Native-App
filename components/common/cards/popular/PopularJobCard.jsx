e6ac2f4d28c668720860af68e8fb8e587e10d1ff
shoaiblatif22 <shoaiblatif11@gmail.com>
2023-09-02 22:54:34 +0100

    Added in sampleData. When hard coding values, it renders properly with the sample company and sample data. There is clearly an issue with API and how it renders to app.

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -2,26 +2,37 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './popularjobcard.style'
 
+
+const sampleData = {
+  job_id: 1,
+  employer_logo: 'https://www.pngwing.com/en/free-png-spjgq', // Replace with a valid image URL
+  employer_name: 'Sample Company',
+  job_title: 'Sample Job Title',
+};
+
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
-  console.log(item)
+
+
+  const dataToRender = sampleData;
+
   return (
     <TouchableOpacity
-      key={item.job_id}
-      style={styles.container(selectedJob, item)}
-      onPress={() => handleCardPress(item)}
+      key={dataToRender.job_id}
+      style={styles.container(selectedJob, dataToRender)}
+      onPress={() => handleCardPress(dataToRender)}
     >
-       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+       <TouchableOpacity style={styles.logoContainer(selectedJob, dataToRender)}>
         <Image
-          source={{ uri: item.employer_logo }}
+          source={{ uri: dataToRender.employer_logo }}
           resizeMode="contain"
           style={styles.logoImage} />
 
 
       </TouchableOpacity>
-      <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text> 
+      <Text style={styles.companyName} numberOfLines={1}>{dataToRender.employer_name}</Text> 
 
-      <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
-        {item.job_title}
+      <Text style={styles.jobName(selectedJob, dataToRender)} numberOfLines={1}>
+        {dataToRender.job_title}
       </Text>
 
 
