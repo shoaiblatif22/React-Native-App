a4f12b2584eb7b6b3bc8e8abe0c4f85a7796a40d
shoaiblatif22 <shoaiblatif11@gmail.com>
2023-09-02 23:09:11 +0100

    Added in location of jobs under text. Placeholder size increases and it has been tested that it works with sample data

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -3,39 +3,40 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 import styles from './popularjobcard.style'
 
 
-const sampleData = {
-  job_id: 1,
-  employer_logo: 'https://www.pngwing.com/en/free-png-spjgq', // Replace with a valid image URL
-  employer_name: 'Sample Company',
-  job_title: 'Sample Job Title',
-};
+// const sampleData = {
+//   job_id: 1,
+//   employer_logo: 'https://www.pngwing.com/en/free-png-spjgq', // Replace with a valid image URL
+//   employer_name: 'Sample Company',
+//   job_title: 'Sample Job Title',
+// };
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
 
 
-  const dataToRender = sampleData;
+  // const item = sampleData;
 
   return (
     <TouchableOpacity
-      key={dataToRender.job_id}
-      style={styles.container(selectedJob, dataToRender)}
-      onPress={() => handleCardPress(dataToRender)}
+      key={item.job_id}
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
     >
-       <TouchableOpacity style={styles.logoContainer(selectedJob, dataToRender)}>
+       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
-          source={{ uri: dataToRender.employer_logo }}
+          source={{ uri: item.employer_logo }}
           resizeMode="contain"
           style={styles.logoImage} />
 
 
       </TouchableOpacity>
-      <Text style={styles.companyName} numberOfLines={1}>{dataToRender.employer_name}</Text> 
+      <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text> 
 
-      <Text style={styles.jobName(selectedJob, dataToRender)} numberOfLines={1}>
-        {dataToRender.job_title}
-      </Text>
-
-
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+        {item.job_title}
+        <Text style={styles.location}>{item.job_country}</Text>
+        </Text>
+      </View>   
 
     </TouchableOpacity>
   )
