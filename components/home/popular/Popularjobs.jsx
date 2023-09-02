b6a127568976338c4706d6cff46e1feab1a619e8
shoaiblatif22 <shoaiblatif11@gmail.com>
2023-09-02 17:27:54 +0100

    Imported files for popular jobs. Added in useRouter. Added in styling for fonts. Added in btn for show all but no functionality yet

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator, TouchableOpacityBase } from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './popularjobs.style'
+import { COLORS, SIZES } from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>          
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
