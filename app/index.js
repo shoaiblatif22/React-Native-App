b918609fe00db819e26339676b8ba1ed43b17ca1
shoaiblatif22 <shoaiblatif11@gmail.com>
2023-09-02 12:40:15 +0100

    Added in all the imports from expo-router, constants and components

--- app/index.js
+++ app/index.js
@@ -1,4 +1,10 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+
 
 const Home = () => {
     return (
