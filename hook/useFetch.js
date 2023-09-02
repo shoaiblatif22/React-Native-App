e838ec0a5fe5f469e81f10bdb631f9722c167e78
shoaiblatif22 <shoaiblatif11@gmail.com>
2023-09-02 18:20:08 +0100

    Added in fetchData function to take an options object as its argument which specifics the url of API, http method and the headers. Ive put the fetch data function using the axios library to make the http request and using await so that it resolves. I then used a try and catch error to catch any errors. The useEffect hook called the fetchData function every time the component is updated which ensures the data is up to date.

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -21,4 +21,26 @@ const useFetch = (endpoint) => {
    params: { ...query },
 };
 
+const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+        const response = await axios.request(options); 
+            setData(response.data.data);
+            setIsLoading(false);
+
+        } catch (error) { 
+            setError(error);
+            alert('There was an error')
+
+        } finally { 
+            setIsLoading(false);
+
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 }
+
