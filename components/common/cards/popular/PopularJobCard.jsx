518aafd1bde561d8818db35fbb19303202c314ac
shoaiblatif22 <shoaiblatif11@gmail.com>
2023-11-18 16:26:15 +0000

    Finished UI/UX for jobCards.

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -18,8 +18,8 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
         <Image 
           source={{ uri: checkImageURL(item.employer_logo) 
             ? item.employer_logo
-            : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg' }}
-
+            : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg' 
+          }}
           resizeMode="contain"
           style={styles.logoImage}
         />
--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -34,6 +34,9 @@ const Nearbyjobs = () => {
         ) : (
           data?.map((job) => (
             <NearbyJobCard
+            job={job}
+            key={`nearby-job-${job?.job_id}`}
+            handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
             
             />
           ))
