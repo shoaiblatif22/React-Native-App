52946675fb72e38c48867c513d2e51a764df56bb
shoaiblatif22 <shoaiblatif11@gmail.com>
2023-11-22 20:38:23 +0000

    Added in screenbutton and callback function for it. Added in ScrollView for refreshing jobs. Next stage is to create a useState

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -29,7 +29,40 @@ const JobDetails = () => {
   const { data, isLoading, error, refetch } = useFetch('job-details', { job_id: params.id })
 
   return (
-    <SafeAreaView style></SafeAreaView>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+      options={{
+        headerStyle: { backgroundColor: COLORS.lightWhite },
+        headerShadowVisible: false,
+        headerBackVisible: false,
+        headerLeft: () => (
+          <ScreenHeaderBtn
+            iconUrl={icons.left}
+            dimension="60%"
+            handlePress={() => router.back()}
+          />
+        ),
+
+          headerRight: () => (
+          <ScreenHeaderBtn
+            iconUrl={icons.share}
+            dimension="60%"
+          />
+        ),
+
+        headerTitle: ''
+      }}
+      />
+
+      <>
+        <ScrollView showsVerticalScrollIndicator={false} 
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
+
+        </ScrollView>
+      </>
+
+      
+    </SafeAreaView>
   )
 }
 
