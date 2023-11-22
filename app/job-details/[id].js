16a7fa302d50ec281a7100d12fccb540a381deb2
shoaiblatif22 <shoaiblatif11@gmail.com>
2023-11-22 20:10:53 +0000

    Added in imports for JobDetails. Added in parameters and used GlobalSearchParams as SearchParams is depracted. Added in router. Added in useFetch hook with the id paramters.

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,9 +1,35 @@
 import React from 'react'
-import { Text } from 'react-native'
+
+import { 
+  Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl 
+} from 'react-native'
+
+import { 
+  Stack, useRouter, useGlobalSearchParams 
+} from 'expo-router'
+
+import { 
+  useCallback, useState 
+} from 'react'
+
+import { 
+  Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specfics 
+} from '../../components'
+
+import {
+  COLORS, icons, SIZES 
+} from '../../constants'
+
+import useFetch from '../../hook/useFetch'
 
 const JobDetails = () => {
+  const params = useGlobalSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch('job-details', { job_id: params.id })
+
   return (
-    <div>JobDetails</div>
+    <SafeAreaView style></SafeAreaView>
   )
 }
 
